7fa873ef8a11d646f38eb7a69a1c59e56285137d
Théo VIGNERON <t.vigneron@boostmymail.com>
2022-03-16 20:36:13 +0100

    .

--- react-webpack-typescript-starter-main/src/components/Case/Case.tsx
+++ react-webpack-typescript-starter-main/src/components/Case/Case.tsx
@@ -9,5 +9,5 @@ export const Case = ({ width = 50, height = 50}) =>{
     const [show, setShow] = useState(false)
     return ( <div style={{ background: show ? '#FFF' : '#000', height: height, width: width }}></div> 
     )
-
+ 
 }
